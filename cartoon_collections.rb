a18e0760db295c09ed1c1e426bdfa9fca063288b
Allene Norton <norton.allene@gmail.com>
2020-06-24 18:25:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  planeteer_calls.max_by(4) do |x|
+  !!planeteer_calls.max_by(4) do |x|
     x.length 
   end
 end
